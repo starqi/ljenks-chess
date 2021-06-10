8d60825e6d16ff56c30a97ba6217f05131624ac0
starqi <helloimzirui@gmail.com>
2021-06-10 14:47:46 -0400

    Add drag/drop UI.

--- src/www/index.js
+++ src/www/index.js
@@ -3,6 +3,9 @@ import * as wasm from './node_modules/chess_bs';
 
 class Board {
     constructor() {
+
+        this.draggedImage = null;
+
         this.main = wasm.Main.new();
         this.LEN = 85;
 
@@ -12,6 +15,14 @@ class Board {
         ];
 
         this.board = document.getElementById('board');
+        this.board.addEventListener('mousedown', this.onBoardMouseDown.bind(this));
+        this.board.addEventListener('mousemove', this.onBoardMouseMove.bind(this));
+        this.board.addEventListener('mouseup', this.onBoardMouseUp.bind(this));
+
+        this.dragged = document.getElementById('dragged');
+        this.dragged.width = this.LEN;
+        this.dragged.height = this.LEN;
+
         this.squareImages = [];
 
         for (let i = 0; i < 8; ++i) {
@@ -40,6 +51,52 @@ class Board {
             this.board.append(rowElement);
             this.squareImages.push(imageRow);
         }
+    }
+
+    onBoardMouseDown(e) {
+        e.preventDefault();
+
+        const r = this.board.getBoundingClientRect();
+        const boardX = e.clientX - r.left;
+        const boardY = e.clientY - r.top;
+
+        const squareX = (boardX / this.LEN) >>> 0;
+        const squareY = (boardY / this.LEN) >>> 0;
+
+        const row = this.squareImages[squareY];
+        if (row === undefined) return;
+        const image = row[squareX];
+        if (image === undefined || image.style.visibility === 'hidden') return;
+
+        image.style.visibility = 'hidden';
+        this.draggedImage = image;
+        this.dragged.src = image.src;
+        this.dragged.style.visibility = 'visible';
+        this.trySyncDragged(e);
+    }
+
+    trySyncDragged(e) {
+        if (this.draggedImage !== null) {
+            const r = this.board.getBoundingClientRect();
+            const boardX = e.clientX - r.left;
+            const boardY = e.clientY - r.top;
+
+            this.dragged.style.left = boardX - this.LEN / 2.0;
+            this.dragged.style.top = boardY - this.LEN / 2.0;
+        }
+    }
+
+    onBoardMouseMove(e) {
+        e.preventDefault();
+        this.trySyncDragged(e);
+    }
+
+    onBoardMouseUp(e) {
+        e.preventDefault();
+
+        this.draggedImage.style.visibility = 'visible';
+        this.draggedImage = null;
+        this.dragged.style.visibility = 'hidden';
     }
 
     setSquareFromWasm(row, col) {
