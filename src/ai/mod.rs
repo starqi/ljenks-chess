d118b15ce4fc864d9d1f8302367f6b11d07d3a55
starqi <starqi@live.ca>
2025-12-21 14:50:27 -0800

    Review refactor no move on LessThan memo type. Add more memo stats.

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -16,7 +16,8 @@ pub struct Ai {
     temp_moves: MoveList,
     af_boards: AttackFromBoards,
     memo: HashMap<u64, MemoData>,
-    memo_hits: usize,
+    useful_memo_hits: usize,
+    hash_move_memo_hits: usize,
     fast_found_hits: usize,
     node_counter: u64,
     start_ms: u128,
@@ -32,8 +33,9 @@ enum SingleMoveResult { NewAlpha(i32), BetaCutOff(i32), NoEffect }
 // TODO Refactor these 2 classes, Clone on enum?
 
 /// Stored move and eval is the best move at the memo position.
+/// For `LessThan`, it seems there is no good move to record, so only an eval is stored.
 #[derive(Clone)]
-enum MemoType { LessThan(MoveWithEval), Exact(MoveWithEval), GreaterThan(MoveWithEval) }
+enum MemoType { LessThan(), Exact(MoveWithEval), GreaterThan(MoveWithEval) }
 
 /// (Score which can be exact or lower or upper bound depending on type, remaining depth, type, age)
 #[derive(Clone)]
@@ -53,7 +55,8 @@ impl Ai {
             temp_moves: MoveList::new(50),
             af_boards: AttackFromBoards::new(),
             memo: HashMap::new(),
-            memo_hits: 0,
+            useful_memo_hits: 0,
+            hash_move_memo_hits: 0,
             fast_found_hits: 0,
             node_counter: 0,
             start_ms: 0,
@@ -67,7 +70,7 @@ impl Ai {
     fn get_leading_move(&self) -> Option<(&MoveWithEval, i8)> {
         match self.memo.get(&self.test_board.get_hash()) {
             // In this context, fail high means checkmate
-            Some(MemoData(_, depth, MemoType::GreaterThan(best_move) | MemoType::Exact(best_move) | MemoType::LessThan(best_move), _)) => {
+            Some(MemoData(_, depth, MemoType::GreaterThan(best_move) | MemoType::Exact(best_move), _)) => {
                 Some((best_move, *depth))
             },
             _ => {
@@ -124,11 +127,18 @@ impl Ai {
             console_log!("No move");
             None
         };
-        console_log!("Memo hits - {}, size - {}, fast found - {}", self.memo_hits, self.memo.len(), self.fast_found_hits);
+        console_log!(
+            "Useful memo hits - {}, hash move memo hits - {}, size - {}, fast found - {}",
+            self.useful_memo_hits,
+            self.hash_move_memo_hits,
+            self.memo.len(),
+            self.fast_found_hits
+        );
         console_log!("Nodes - {}, NPS - {}", self.node_counter, (self.node_counter as f64 / ((now() - self.start_ms) as f64 / 1000.)).round());
 
         self.node_counter = 0;
-        self.memo_hits = 0;
+        self.useful_memo_hits = 0;
+        self.hash_move_memo_hits = 0;
         self.fast_found_hits = 0;
         // TODO Memo experiment... statistics...
         //self.memo.clear();
@@ -183,20 +193,17 @@ impl Ai {
             // If the memoized move has the precision we want, use its score
             if *saved_depth >= remaining_depth {
                 match memo_type {
-                    MemoType::LessThan(_) => {
+                    MemoType::LessThan() => {
                         if *saved_num <= alpha {
-                            self.memo_hits += 1;
                             return (Some(memo_type), Some(alpha));
                         }
                     },
                     MemoType::GreaterThan(_) => {
                         if *saved_num >= beta { 
-                            self.memo_hits += 1;
                             return (Some(memo_type), Some(beta));
                         }
                     },
                     MemoType::Exact(_) => {
-                        self.memo_hits += 1;
 
                         if *saved_num < alpha {
                             return (Some(memo_type), Some(alpha)); 
@@ -231,6 +238,7 @@ impl Ai {
         let mut alpha = initial_alpha;
 
         let score = if let (_, Some(adjusted_score)) = self.find_memo_score(0, alpha, beta) {
+            self.useful_memo_hits += 1;
             return adjusted_score; // No score multiplier necessary
         } else {
             score_multiplier * evaluation::evaluate(&self.test_board, &mut self.af_boards)
@@ -288,63 +296,59 @@ impl Ai {
         beta: i32,
         moves_start: usize
     ) -> i32 {
+        const NEW_ALPHA_I_NEVER_SET: i32 = -1;
+        const NEW_ALPHA_I_HASH_MOVE: i32 = -2;
 
         if remaining_depth <= 0 {
             return self.qsearch(10, initial_alpha, beta, moves_start);
         }
-
         if self.increment_node_check_termination() { return initial_alpha; } // Chain force beta cutoff in all parents; checked by assertions (2)
 
         let mut alpha = initial_alpha;
-
-        const NEW_ALPHA_I_NEVER_SET: i32 = -1;
-        const NEW_ALPHA_I_HASH_MOVE: i32 = -2;
-
         let mut new_alpha_i: i32 = NEW_ALPHA_I_NEVER_SET;
         // When `new_alpha_i` is `NEW_ALPHA_I_HASH_MOVE`, the hash move can be found here
         let mut hash_move: Option<MoveWithEval> = None;
 
-        match self.find_memo_score(remaining_depth, alpha, beta) {
+        let memo_move_suggestion: Option<MoveWithEval> = match self.find_memo_score(remaining_depth, alpha, beta) {
             (_, Some(adjusted_score)) => { // Use memoized move
+                self.useful_memo_hits += 1;
                 return adjusted_score;
             },
-            (Some(memo_type), None) => { // Memoized move is not precise enough, try using it as the first best move
-
-                // Clone the move, unlike if the move is on move list, because memo updates will overwrite the same memory. Also, we can move it into the memo.
-                let move_clone = if let MemoType::Exact(m) | MemoType::GreaterThan(m) = memo_type {
-                    Some(m.clone())
-                } else {
-                    // (1) For fail low memo entries, currently the move is a random move so we can't use it as the best move (but it doesn't have to be that way TODO)
-                    None
-                };
-
-                if let Some(m) = move_clone {
-                    // Reminder: No null window, because this is our best move candidate, hence it is not expected to fail low
-                    match self.negamax_try_move(remaining_depth, alpha, false, beta, &m, moves_start) {
-                        SingleMoveResult::BetaCutOff(score) => {
-                            if self.terminated {
-                                return initial_alpha; // See (2)
-                            } else {
-                                self.insert_memo(MemoData(score, remaining_depth, MemoType::GreaterThan(m), 0));
-                                return beta;
-                            }
-                        },
-                        SingleMoveResult::NewAlpha(score) => {
-                            // The move loop below will begin not with the alpha provided from caller,
-                            // but with the proven better alpha re-examined at full depth from the memo, which is also an exact score.
-                            alpha = score;
-                            new_alpha_i = NEW_ALPHA_I_HASH_MOVE;
-                            hash_move = Some(m);
-                        },
-                        SingleMoveResult::NoEffect => {
-                            // The memoized move was not very good after examining it full depth, begin normal loop through moves.
-                        }
-                    };
-                    assert!(!self.terminated);
-                }
+            (Some(MemoType::Exact(m) | MemoType::GreaterThan(m)), None) => {
+                // Memoized move is not precise enough, try using it as the first best move.
+                // Must clone the move, because recursive memo updates touch the same memory.
+                Some(m.clone())
             },
-            _ => {}
+            _ => None
         };
+
+        if let Some(m) = memo_move_suggestion {
+
+            // Reminder: No null window, because this is our best move candidate, hence it is not expected to fail low
+            match self.negamax_try_move(remaining_depth, alpha, false, beta, &m, moves_start) {
+                SingleMoveResult::BetaCutOff(score) => {
+                    self.hash_move_memo_hits += 1;
+                    if self.terminated {
+                        return initial_alpha; // See (2)
+                    } else {
+                        self.insert_memo(MemoData(score, remaining_depth, MemoType::GreaterThan(m), 0));
+                        return beta;
+                    }
+                },
+                SingleMoveResult::NewAlpha(score) => {
+                    self.hash_move_memo_hits += 1;
+                    // The move loop below will begin not with the alpha provided from caller,
+                    // but with the proven better alpha re-examined at full depth from the memo, which is also an exact score.
+                    alpha = score;
+                    new_alpha_i = NEW_ALPHA_I_HASH_MOVE;
+                    hash_move = Some(m);
+                },
+                SingleMoveResult::NoEffect => {
+                    // The memoized move was not very good after examining it full depth, begin normal loop through moves.
+                }
+            };
+            assert!(!self.terminated);
+        }
 
         // Generate moves and order
         self.moves_buf.write_index = moves_start;
@@ -392,7 +396,7 @@ impl Ai {
             let mut less_depth_amount = (-((remaining_depth > 3 && new_alpha_i != NEW_ALPHA_I_NEVER_SET) as i32)) 
                 & min(-((m_score < 100) as i32) & ((100 - m_score) >> 5), 3);
 
-            // TODO Statistics for research
+            // TODO Statistics for research, number of retries
             loop {
                 let r = self.negamax_try_move(
                     remaining_depth - (less_depth_amount as i8), 
@@ -443,8 +447,7 @@ impl Ai {
         } else if new_alpha_i >= 0 {
             self.insert_memo(MemoData(alpha, remaining_depth, MemoType::Exact(self.moves_buf.v()[new_alpha_i as usize].clone()), 0));
         } else {
-            // See (1)
-            self.insert_memo(MemoData(alpha, remaining_depth, MemoType::LessThan(self.moves_buf.v()[0].clone()), 0));
+            self.insert_memo(MemoData(alpha, remaining_depth, MemoType::LessThan(), 0));
         }
 
         alpha
@@ -468,7 +471,7 @@ impl Ai {
 
         if do_null_window {
 
-            // PVS intuition
+            // [PVS intuition]
             // In the ideal alpha-beta setup, the first move is the best move, set a new alpha and no new alpha is set again.
             // In this case, we do the same alpha-beta (no null window) for the first move, followed by as small a window as possible
             // for subsequent moves (faster), betting on fail-lows. (If we do null window on first move, then it'll frequently re-search.)
